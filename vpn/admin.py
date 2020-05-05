85a38e241433988a128a8f9434096fbea38c8f96
Adonis Gaitatzis <backupbrain@gmail.com>
2020-05-05 21:45:32 +0300

    adding event log

--- vpn/admin.py
+++ vpn/admin.py
@@ -3,7 +3,7 @@ from .models import (
     VpnClientEvent
 )
 
-'''
+
 @admin.register(VpnClientEvent)
 class VpnClientEventAdmin(admin.ModelAdmin):
     """Admin represtation of VpnClientEvent."""
@@ -11,4 +11,3 @@ class VpnClientEventAdmin(admin.ModelAdmin):
     list_display = ('error', 'client_software_version', 'client_os', 'public_key')
     ordering = ('error', 'public_key', 'client_os', 'client_software_version')
     list_filter = ('error', 'client_os', 'client_software_version')
-'''
