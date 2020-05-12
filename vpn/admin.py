eb757623519e111a5d2f489b9289b2ffe7e942ca
Adonis Gaitatzis <backupbrain@gmail.com>
2020-05-12 21:13:48 +0300

    updating search

--- vpn/admin.py
+++ vpn/admin.py
@@ -9,6 +9,6 @@ class VpnClientEventAdmin(admin.ModelAdmin):
     """Admin represtation of VpnClientEvent."""
 
     list_display = ('error', 'created_at', 'client_os', 'public_key')
-    ordering = ('created_at', 'error', 'public_key', 'client_os')
+    ordering = ('-created_at', 'error', 'public_key', 'client_os')
     list_filter = ('error', 'client_os', 'client_software_version')
     search_fields = ('error', 'public_key', 'client_os', 'debugging_messages')
--- vpn/views_api_0_1.py
+++ vpn/views_api_0_1.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from ipaddress import ip_address
 from .serializers_0_1 import (
     VpnClientEventSerializer,
 )
@@ -22,12 +23,30 @@ class VpnClientEventRestApiModelViewSet(CsrfExemptMixin, ModelViewSet):
     serializer_class = VpnClientEventSerializer
     pagination_class = None
 
+    def get_client_ip(self, request):
+        """Get the client IP address."""
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+
     def get_queryset(self):
         """Override the queryset."""
         return None
 
     def add_loggable_event(self, request):
         """Save an event that happened on a VPN API client such as crashes or routing problems."""
+        ip = None
+        try:
+            ip = ip_address(self.get_client_ip(request))
+            if ip.version == 4:
+                request.data['ip4_address'] = str(ip)
+            else:
+                request.data['ip6_address'] = str(ip)
+        except ValueError:
+            pass
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
