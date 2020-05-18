f930ee60b4819543bfc47833122f68f084712968
Adonis Gaitatzis <backupbrain@gmail.com>
2020-05-18 10:57:41 +0300

    adding api version 0.1 backward compatible urls

--- vpn/urls_api_0_1.py
+++ vpn/urls_api_0_1.py
@@ -8,6 +8,8 @@ from .views_api_0_1 import (
 app_name = 'vpn_api_0_1'
 
 urlpatterns = [
+    path('client/event/', VpnClientEventRestApiModelViewSet.as_view({'post': 'add_loggable_event'})),
+
     path('clients/events/', VpnClientEventRestApiModelViewSet.as_view({'post': 'add_loggable_event'})),
     path('clients/versions/<client_os>/', ClientSoftwareVersionRestApiView.as_view({'get': 'get_latest_version', 'post': 'add_new_version'})),
     path('servers/', CjdnsVpnServerRestApiView.as_view({'get': 'list_servers', 'post': 'add_server'})),
